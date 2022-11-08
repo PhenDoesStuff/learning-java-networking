f8462d111bd8e4f9f0f5ff0ed93deecbe56b0c2f
Stephen Montague <stephen.montague@deposco.com>
2022-11-08 14:42:47 -0500

    Moved code around so that multiple clients can be created at the same time. We see an issue here because it's all running on one thread.

--- EchoServer/src/com/practice/Main.java
+++ EchoServer/src/com/practice/Main.java
@@ -11,15 +11,20 @@ public class Main {
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(8080)) {
-            Socket socket = serverSocket.accept();
-
-            System.out.println("Client connected.");
-
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
             while(true) {
+                Socket socket = serverSocket.accept();
+
+                System.out.println("Client connected.");
+
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
                 String echoString = input.readLine();
+                try {
+                    Thread.sleep(15000);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread interrupted.");
+                }
                 if (echoString.equals("exit")) {
                     break;
                 }
