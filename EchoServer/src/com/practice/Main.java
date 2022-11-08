6d34e05448b8ac02cd5ffdfc395d33f2699a5beb
Stephen Montague <stephen.montague@deposco.com>
2022-11-08 14:49:40 -0500

    This main class now calls Echoer and should create more threads.

--- EchoServer/src/com/practice/Main.java
+++ EchoServer/src/com/practice/Main.java
@@ -1,37 +1,17 @@
 package com.practice;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Main {
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(8080)) {
-            while(true) {
-                Socket socket = serverSocket.accept();
-
-                System.out.println("Client connected.");
-
-                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
-                String echoString = input.readLine();
-                try {
-                    Thread.sleep(15000);
-                } catch (InterruptedException e) {
-                    System.out.println("Thread interrupted.");
-                }
-                if (echoString.equals("exit")) {
-                    break;
-                }
-                output.println("Echo from server: " + echoString);
+            while (true) {
+                new Echoer(serverSocket.accept()).start();
             }
         } catch (IOException e) {
-            System.out.println("Server excepton " + e.getMessage());
+            System.out.println("Server exception " + e.getMessage());
         }
     }
 }
