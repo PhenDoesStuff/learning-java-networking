1ba2c26cf15c553d9eee81db326d123cb2976337
Stephen Montague <stephen.montague@deposco.com>
2022-11-08 13:40:09 -0500

    Improvements to the server so that we can read from the client and respond to it as well.

--- EchoServer/src/com/practice/Main.java
+++ EchoServer/src/com/practice/Main.java
@@ -1,6 +1,9 @@
 package com.practice;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,9 +12,21 @@ public class Main {
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(8080)) {
             Socket socket = serverSocket.accept();
+
             System.out.println("Client connected.");
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            while(true) {
+                String echoString = input.readLine();
+                if (echoString.equals("exit")) {
+                    break;
+                }
+                output.println("Echo from server: " + echoString);
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Server excepton " + e.getMessage());
         }
     }
 }
